0e3943edfeb70950111d86afd31b9314450cba2f
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-05-30 12:38:49 +0200

    Initial commit

---
+++ Foodam/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodam
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
