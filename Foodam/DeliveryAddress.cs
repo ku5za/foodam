3c32d1507cf33199a935e3211294b68ed87b1283
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-05-30 21:15:36 +0200

    Add DeliveryAddres class

---
+++ Foodam/DeliveryAddress.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Foodam
+{
+	class DeliveryAddress
+	{
+		#region fields
+		InputValidator StreetValidator;
+		InputValidator PostalCodeValidator;
+		InputValidator CityNameValidator;
+		#endregion
+
+		#region properties
+		public string Street
+		{
+			get => Street;
+			set
+			{
+				if(StreetValidator.IsValid(value))
+				{
+					Street = value;
+				}
+				else
+				{
+					throw new Exception("Wprowadzono niepoprawną nazwę ulicy");
+				}
+			}
+		}
+
+		public string PostalCode
+		{
+			get => PostalCode;
+			set
+			{
+				if(PostalCodeValidator.IsValid(value))
+				{
+					PostalCode = value;
+				}
+				else
+				{
+					throw new Exception("Wprowadzono nieprawidłowy kod pocztowy");
+				}
+			}
+		}
+
+		public string City
+		{
+			get => City;
+			set
+			{
+				if (CityNameValidator.IsValid(value)) 
+				{
+					City = value;
+				}
+				else
+				{
+					throw new Exception("Wprowadzono niepoprawną nazwę miasta");
+				}
+			}
+		}
+		#endregion
+
+		#region methods
+		public DeliveryAddress()
+		{
+			StreetValidator = new StreetValidator();
+			PostalCodeValidator = new PostalCodeValidator();
+			CityNameValidator = new CityValidator();
+		}
+
+		public string GetDeliveryAddress()
+		{
+			return $"{Street}, {PostalCode} {City}";
+		}
+		#endregion
+	}
+}
