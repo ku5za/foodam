ac02aff3d2748c31ccd93f1c7bc1eeb1b6c64ba2
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-05-31 21:41:54 +0200

    Add Restaurant's class initial implementation

---
+++ Foodam/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodam
+{
+	public class Restaurant
+	{
+		#region field
+		private string name;
+		private Address address;
+		private Menu menu;
+		#endregion
+
+		#region properties
+		public string Name => name;
+		public Address Address => address;
+		public Menu Menu => menu;
+		#endregion
+
+		#region methods
+		public Restaurant(string name, Address address, Menu menu)
+		{
+			this.name = name;
+			this.address = address;
+			this.menu = menu;
+		}
+		#endregion
+	}
+}
