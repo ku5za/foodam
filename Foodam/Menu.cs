253c5dfdea006963d8153e2ecd52b4ffd8f08d14
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-05-31 14:55:05 +0200

    Add Menu's class initial implementation

---
+++ Foodam/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodam
+{
+	public class Menu
+	{
+		#region fields
+		private List<MenuItem> menuItems = new List<MenuItem>();
+		#endregion
+
+		#region methods
+		public Menu(List<MenuItem> menuItems)
+		{
+			this.menuItems = menuItems;
+		}
+
+		public Menu()
+			: this(new List<MenuItem>())
+		{ }
+
+		public void addMenuItem(MenuItem menuItem)
+		{
+			menuItems.Add(menuItem);
+		}
+
+		public MenuItem GetMenuItem(int index)
+		{
+			if (index >= 0 && menuItems.Count > index)
+			{
+				return menuItems[index];
+			}
+			else
+			{
+				throw new Exception("There's no Menu Item with such index");
+			}
+		}
+
+		public int GetMenuLength()
+		{
+			return menuItems.Count;
+		}
+		#endregion
+	}
+}
