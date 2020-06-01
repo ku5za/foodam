02fb794c7c0f0e7bb059c03444abae6681d3c610
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-01 19:59:05 +0200

    Add Discount implementation class - DiscountPercentage

---
+++ Foodam/DiscountPercentage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodam
+{
+	public class DiscountPercentage : Discount
+	{
+		#region fields
+		private double discountPercent;
+		#endregion
+
+		#region methods
+		public DiscountPercentage(double discountPercent, List<string> discountComponentialMenuItemClasses)
+			: base(discountComponentialMenuItemClasses)
+		{
+			this.discountPercent = discountPercent;
+		}
+
+		public DiscountPercentage(double discountPercent, params string[] discountComponentialMenuItemClasses)
+			: base(discountComponentialMenuItemClasses)
+		{
+			this.discountPercent = discountPercent;
+		}
+
+		public override double CalculateDecreasedPrice(List<MenuItem> menuItems)
+		{
+			double totalPrice = 0d;
+
+			foreach(var menuItem in menuItems)
+			{
+				totalPrice += menuItem.Price;
+			}
+
+			return totalPrice * (1.0d - discountPercent);
+		}
+		#endregion
+	}
+}
