757d6d19db86ca85a8879dae5b4c5e9fc2bc6a79
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-01 18:57:07 +0200

    Add Discount's class initial implementation

---
+++ Foodam/Discount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodam
+{
+	public abstract class Discount
+	{
+		private List<string> menuItemClassesOnDiscount;
+
+		public Discount(List<string> menuItemClassesOnDiscount)
+		{
+			this.menuItemClassesOnDiscount = menuItemClassesOnDiscount;
+		}
+
+		public abstract double CalculateDecreasedPrice(List<MenuItem> menuItems);
+	}
+}
