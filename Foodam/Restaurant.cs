90f25390d0c15b744e3150c0266bb30c684dd574
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-01 22:22:29 +0200

    Add Discounts list to Restaurant class

--- Foodam/Restaurant.cs
+++ Foodam/Restaurant.cs
@@ -12,20 +12,23 @@ namespace Foodam
 		private string name;
 		private Address address;
 		private Menu menu;
+		private List<Discount> discounts;
 		#endregion
 
 		#region properties
 		public string Name => name;
 		public Address Address => address;
 		public Menu Menu => menu;
+		public List<Discount> Discounts => discounts;
 		#endregion
 
 		#region methods
-		public Restaurant(string name, Address address, Menu menu)
+		public Restaurant(string name, Address address, Menu menu, List<Discount> discounts)
 		{
 			this.name = name;
 			this.address = address;
 			this.menu = menu;
+			this.discounts = discounts;
 		}
 		#endregion
 	}
