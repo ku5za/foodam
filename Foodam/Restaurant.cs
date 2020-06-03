d9efe3e99e2a5a447fe5a204aa02628c108a364d
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-03 14:51:11 +0200

    Add phoneNumber field to Restaurant

--- Foodam/Restaurant.cs
+++ Foodam/Restaurant.cs
@@ -10,6 +10,7 @@ namespace Foodam
 	{
 		#region field
 		private string name;
+		private string phoneNumber;
 		private Address address;
 		private Menu menu;
 		private List<Discount> discounts;
@@ -17,22 +18,24 @@ namespace Foodam
 
 		#region properties
 		public string Name => name;
+		public string PhoneNumber => phoneNumber;
 		public Address Address => address;
 		public Menu Menu => menu;
 		public List<Discount> Discounts => discounts;
 		#endregion
 
 		#region methods
-		public Restaurant(string name, Address address, Menu menu, List<Discount> discounts)
+		public Restaurant(string name, string phoneNumber, Address address, Menu menu, List<Discount> discounts)
 		{
 			this.name = name;
+			this.phoneNumber = phoneNumber;
 			this.address = address;
 			this.menu = menu;
 			this.discounts = discounts;
 		}
 
-		public Restaurant(string name, Address address, Menu menu, params Discount[] discounts)
-			: this(name, address, menu, new List<Discount>(discounts))
+		public Restaurant(string name, string phoneNumber, Address address, Menu menu, params Discount[] discounts)
+			: this(name, phoneNumber, address, menu, new List<Discount>(discounts))
 		{ }
 		#endregion
 	}
