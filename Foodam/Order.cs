551fe30f82bda6d9232c9f3bad81647860e381ed
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-03 19:49:15 +0200

    Add Order's class initial implementation

---
+++ Foodam/Order.cs
@@ -0,0 +1,48 @@
+﻿using Foodam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases
+{
+	public class Order
+	{
+		#region fields
+		private Address deliveryAddress;
+		private RestaurantContactDetails restaurantContactDetails;
+		private List<MenuItem> orderedMenuItems;
+		private ContactDetails contactDetails;
+		private PaymentDetails paymentDetails;
+		private double totalPrice;
+		#endregion
+
+		#region properties
+		public Address DeliveryAddress => deliveryAddress;
+		public RestaurantContactDetails RestaurantContactDetails => restaurantContactDetails;
+		public List<MenuItem> OrderedMenuItems => orderedMenuItems;
+		public ContactDetails ContactDetails => contactDetails;
+		public PaymentDetails PaymentDetails => paymentDetails;
+		public double TotalPrice => totalPrice;
+		#endregion
+
+		#region methods
+		public Order(
+			Address deliveryAddress,
+			RestaurantContactDetails restaurantContactDetails,
+			List<MenuItem> orderedMenuItems,
+			ContactDetails contactDetails,
+			PaymentDetails paymentDetails,
+			double totalPrice)
+		{
+			this.deliveryAddress = deliveryAddress;
+			this.restaurantContactDetails = restaurantContactDetails;
+			this.orderedMenuItems = orderedMenuItems;
+			this.contactDetails = contactDetails;
+			this.paymentDetails = paymentDetails;
+			this.totalPrice = totalPrice;
+		}
+		#endregion
+	}
+}
