0251bc81085ba5ad8bde848ad2c9c25bde6a7529
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-03 13:47:45 +0200

    Add PaymentDetails' class initial implementation

---
+++ Foodam/PaymentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodam
+{
+	public class PaymentDetails
+	{
+		#region fields
+		private string paymentMethod;
+		#endregion
+
+		#region properties
+		public string PaymentMethod => paymentMethod.Length == 0 ? "Payment method not assigned" : paymentMethod;
+		#endregion
+
+		#region methods
+		public PaymentDetails(string paymentMethod) => this.paymentMethod = paymentMethod;
+		#endregion
+	}
+}
