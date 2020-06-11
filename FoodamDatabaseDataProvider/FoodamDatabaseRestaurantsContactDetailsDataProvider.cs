b236d715287a84048fc4d4fdeced4040b7ba898d
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-11 23:08:57 +0200

    Add data provider for FoodamDatabase

---
+++ FoodamDatabaseDataProvider/FoodamDatabaseRestaurantsContactDetailsDataProvider.cs
@@ -0,0 +1,57 @@
+﻿using Foodam;
+using InterfaceAdapters;
+using InterfaceAdapters.FoodamDatabaseDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodamDatabaseDataProvider
+{
+	public class FoodamDatabaseRestaurantsContactDetailsDataProvider : RestaurantContactDetailsGateway
+	{
+		private readonly Address address;
+
+		public FoodamDatabaseRestaurantsContactDetailsDataProvider(string street, string postalCode, string city)
+		{
+			address = new Address(street, postalCode, city);
+
+			MatchingCriteria = new RestaurantsContactDetailsMatcher( rcd => rcd.Address.PostalCode == address.PostalCode);
+		}
+
+		public RestaurantsContactDetailsMatcher MatchingCriteria
+		{
+			get;
+			set;
+		}
+
+		public override List<RestaurantContactDetails> GetListOfRestaurantsContactDetails()
+		{
+			RestaurantsTableAdapter restaurantsTableAdapter = new RestaurantsTableAdapter();
+			var restaurantsTable = restaurantsTableAdapter.GetData();
+			List<RestaurantContactDetails> listToReturn = new List<RestaurantContactDetails>();
+
+			foreach(var restaurantContactDetails in restaurantsTable)
+			{
+				var fetchedRestaurantContactDetails = 
+					new RestaurantContactDetails(
+						restaurantContactDetails.RestaurantsName,
+						restaurantContactDetails.PhoneNumber,
+						new Address(
+							restaurantContactDetails.Street,
+							restaurantContactDetails.PostalCode,
+							restaurantContactDetails.City
+						)
+				);
+
+				if (MatchingCriteria(fetchedRestaurantContactDetails))
+				{
+					listToReturn.Add(fetchedRestaurantContactDetails);
+				}
+			}
+
+			return listToReturn;
+		}
+	}
+}
