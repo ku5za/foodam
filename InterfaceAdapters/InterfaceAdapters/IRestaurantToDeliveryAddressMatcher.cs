11c8f47fb02e48d916dca3e73e3b2a620f56f214
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-11 11:12:11 +0200

    Add InterfaceAdapters component

---
+++ InterfaceAdapters/InterfaceAdapters/IRestaurantToDeliveryAddressMatcher.cs
@@ -0,0 +1,10 @@
+﻿using Foodam;
+using System.Collections.Generic;
+
+namespace InterfaceAdapters
+{
+	public interface IRestaurantToDeliveryAddressMatcher
+	{
+		bool IsMatchingRestaurantsContactDetails(Address address, RestaurantContactDetails restaurantContactDetails);
+	}
+}
