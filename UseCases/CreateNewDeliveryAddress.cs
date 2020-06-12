65cf5df9e15d30dfcfd9ffdb3a87ef3cb4118d91
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-12 14:34:38 +0200

    Add CreateNewDeliveryAddress use case

---
+++ UseCases/CreateNewDeliveryAddress.cs
@@ -0,0 +1,25 @@
+﻿using Foodam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases
+{
+	public class CreateNewDeliveryAddress
+	{
+		private readonly IDeliveryAddressProvider provider;
+		public CreateNewDeliveryAddress(IDeliveryAddressProvider deliveryAddressProvider)
+			=> provider = deliveryAddressProvider;
+
+		public Address getNewDeliveryAddress()
+		{
+			string street = provider.GetStreet();
+			string postalCode = provider.GetPostalCode();
+			string city = provider.GetCity();
+
+			return new Address(street, postalCode, city);
+		}
+	}
+}
---
+++ UseCases/IDeliveryAddressProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace UseCases
+{
+	public interface IDeliveryAddressProvider
+	{
+		string GetStreet();
+		string GetPostalCode();
+		string GetCity();
+	}
+ }
