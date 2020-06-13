65a915c0377a514cbae6b512243aabd9660d4c61
Jędrzej Kuszewski <kuszkuszewski@gmail.com>
2020-06-13 22:54:19 +0200

    Delete test code from DeliveryAddressFormPage

--- FoodamWPFDesktopGUI/DeliveryAddressFormPage.xaml.cs
+++ FoodamWPFDesktopGUI/DeliveryAddressFormPage.xaml.cs
@@ -27,41 +27,6 @@ namespace FoodamWPFDesktopGUI
 		public DeliveryAddressFormPage()
 		{
 			InitializeComponent();
-			
-			string textToDisplay = string.Empty;
-
-			Address[] addresses =
-			{
-				new Address("Street1", "87-300", "Brodnica"),
-				new Address("Street2", "00-213", "Warszawa"),
-				new Address("Street3", "00-123", "Warszawa"),
-				new Address("Street4", "40-012", "Katowice"),
-				new Address("Street5", "85-111", "Bydgoszcz"),
-				new Address("Street6", "05-091", "Ząbki")
-			};
-
-			foreach (var address in addresses)
-			{
-				FoodamDatabaseRestaurantsContactDetailsDataProvider dataProvider = 
-					new FoodamDatabaseRestaurantsContactDetailsDataProvider(address.Street, address.PostalCode, address.City);
-				MatchRestaurantsToDeliveryAddress matchRestaurants = 
-					new MatchRestaurantsToDeliveryAddress(address, dataProvider);
-				var matchingRestaurants = matchRestaurants.GetMatchingRestaurantsList();
-
-				textToDisplay += $"Restauracje dla adresu {address.Street}, {address.PostalCode}, {address.City}:\n";
-				foreach (var matchedRestaurant in matchingRestaurants)
-				{
-					textToDisplay += $"{matchedRestaurant.Name}";
-					textToDisplay += $", {matchedRestaurant.PhoneNumber}";
-					textToDisplay += $", {matchedRestaurant.Address.Street}";
-					textToDisplay += $", {matchedRestaurant.Address.PostalCode}";
-					textToDisplay += $", {matchedRestaurant.Address.City}";
-				}
-
-				textToDisplay += "\n";
-			}
-
-			//MatchingRestaurant_TextBox.Text = textToDisplay;
 		}
 
 		private void SearchRestaurants_Button_Click(object sender, RoutedEventArgs e)
